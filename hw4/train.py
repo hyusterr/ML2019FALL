4103c3e88b86e3e8c30814b9a690bfb46074225c
YU-SHIANG HUANG <52968111+hyusterr@users.noreply.github.com>
2019-11-29 23:51:17 +0800

    Add files via upload

--- hw4/train.py
+++ hw4/train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import sys
@@ -11,31 +11,66 @@ from PIL import Image
 import torchvision
 from torch.utils.data import DataLoader, Dataset
 from torchvision.utils import save_image
+from torchsummary import summary
 from torch import nn
 
 
-# In[ ]:
+# In[3]:
 
 
 trainX = np.load(sys.argv[1])
+
+
+# In[6]:
+
+
 trainX = np.transpose(trainX, (0, 3, 1, 2)) / 255. * 2 - 1
 trainX = torch.Tensor(trainX)
+trainX[1].shape
 
 
-# In[ ]:
+# In[7]:
 
 
 use_gpu = torch.cuda.is_available()
 print('I have CUDA: ', use_gpu)
 
 
-# In[ ]:
+# In[8]:
 
 
-# define model
+class hw4_dataset(Dataset):
+    
+    def __init__(self, data, transform):
+        self.data = data
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+
+
+# In[9]:
+
 
 from torch.autograd import Variable
 from torchvision import transforms
+
+num_epochs = 500
+batch_size = 64
+learning_rate = 1e-4
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# dataset = MNIST('./data', transform=img_transform)
+dataloader = DataLoader(trainX, batch_size=batch_size, shuffle=True)
+
 
 class autoencoder(nn.Module):
     def __init__(self):
@@ -95,19 +130,45 @@ class autoencoder(nn.Module):
 
 
 model = autoencoder().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+
+# In[10]:
+
 
 print(model)
 
 
-# In[ ]:
+# In[531]:
 
 
-# load trained models
+# training
 
-model.load_state_dict(torch.load('./3+3conv_autoencoder-with3BN-deeperCONV.pth'))
+model.train()
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img = data
+        img = Variable(img).cuda()
+        # ===================forward=====================
+        output1, output = model(img)
+        loss = criterion(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.5f}'
+          .format(epoch+1, num_epochs, loss.data))
+#     if epoch % 10 == 0:
+#         pic = to_img(output.cpu().data)
+#         save_image(pic, './dc_img/image_{}.png'.format(epoch))
+
+torch.save(model.state_dict(), './3+3conv_autoencoder-with3BN-deeperCONV.pth')
 
 
-# In[ ]:
+# In[13]:
 
 
 # get 1st dim reduction result
@@ -126,7 +187,13 @@ for i in trainX:
 X = np.array( X )
 
 
-# In[ ]:
+# In[14]:
+
+
+print(X.shape)
+
+
+# In[218]:
 
 
 from sklearn.decomposition import KernelPCA
@@ -136,17 +203,17 @@ KPCA_X = transformer.fit_transform(X)
 # KPCA_X.lambda_
 
 
-# In[ ]:
+# In[48]:
 
 
 from sklearn.manifold import TSNE
 
 X_embedded = TSNE(n_components=2).fit_transform(KPCA_X)
 
-print( X_embedded.shape )
+X_embedded.shape
 
 
-# In[ ]:
+# In[240]:
 
 
 # clustering
@@ -158,11 +225,21 @@ print( kmeans.labels_[:10] )
 # print( kmeans.cluster_centers_ )
 
 
-# In[ ]:
+# In[92]:
 
 
-# need reverse
-lst = [int(i) for i in kmeans.labels_]
+from sklearn.cluster import AgglomerativeClustering
+
+agglo_cluster = AgglomerativeClustering( n_clusters=2 )
+agglo_cluster.fit(X_embedded)
+
+print(agglo_cluster.labels_)
+
+
+# In[224]:
+
+
+lst = [int(i) for i in agglo_cluster.labels_]
 
 lst = [str(i) + ',' + str(lst[i]) + '\n' for i in range(len(lst))]
 
